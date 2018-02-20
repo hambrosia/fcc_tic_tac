a168c29ac500e6cf40c074cfd705f5fab93ed090
Matthew Hambro <matthew.hambro@gmail.com>
2018-02-19 18:47:01 -0800

    added dom library
    should add support for input

--- sketch.js
+++ sketch.js
@@ -69,12 +69,7 @@ function draw(){
   }
 }
 
-function tuouchStarted(){
-  alert('touch detected');
-}
-
 function mouseClicked() {
-  alert('click detected');
   //ROW 1
   if( (mouseX > 85) && (mouseX < 115) && (mouseY >85) && (mouseY < 115)){
     if(gameStatus[0][0] == 0){
